f5c9d87b720fce3a7e54871d4a3d48e277165af9
Ankit Yadav <ankityadavpvs@gmail.com>
2026-04-17 21:30:30 +0530

    create a login ui

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -20,16 +20,18 @@ const Login = () => {
   }
 
   return (
-    <form
+     <div className="w-full max-w-100 ml-100 m-25 bg-gray-300 border border-blue-100 rounded-xl p-6 py-10 sm:p-8 flex flex-col gap-4">
+      <form  
       onSubmit={handleSubmit(handleLogin)}
       action="" className='flex items-center gap-5  justify-center ap-5  flex-col '>
-      <input {...register("email")} type="email" placeholder='email.com' className='outline-1 bordered ' />
-      <input {...register("password")} type="password" placeholder='Password' className='outline-1 bordered ' />
-      <button>login </button>
+      <input {...register("email")} type="email" placeholder='email.com' className="h-9 px-3 rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all w-full" />
+      <input {...register("password")} type="password" placeholder='Password' className="h-9 px-3 rounded-lg border border-gray-200 bg-white font-medium text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all w-full" />
+      <button className="mt-2 h-10 w-full rounded-lg bg-gray-500   font-medium">login </button>
 
 
       <p>Don't have Account <Link className='text-blue-500' to="/register">Register </Link></p>
     </form>
+     </div>
   )
 }
 
