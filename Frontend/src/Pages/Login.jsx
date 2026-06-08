273440df16661b6a382eee0f994b70b2ce4752c6
Ankit Yadav <ankityadavpvs@gmail.com>
2026-06-08 18:14:41 +0530

    changing in backend

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -20,43 +20,7 @@ const Login = () => {
   }
 
   return (
-//  <div className="w-full min-h-screen flex items-center justify-center px-4">
-//   <div className="w-full max-w-md bg-gray-300 border border-blue-100 rounded-xl p-6 sm:p-8">
-    
-//     <form  
-//       onSubmit={handleSubmit(handleLogin)}
-//       className="flex flex-col gap-4"
-//     >
-      
-//       <input
-//         {...register("email")}
-//         type="email"
-//         placeholder="email.com"
-//         className="h-10 px-3 rounded-lg border border-gray-200 bg-white text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 w-full"
-//       />
 
-//       <input
-//         {...register("password")}
-//         type="password"
-//         placeholder="Password"
-//         className="h-10 px-3 rounded-lg border border-gray-200 bg-white text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 w-full"
-//       />
-
-//       <button className="h-10 w-full rounded-lg bg-gray-500 text-white font-medium hover:bg-gray-600 transition">
-//         Login
-//       </button>
-
-//       <p className="text-sm text-center">
-//         Don't have Account?{" "}
-//         <Link className="text-blue-500" to="/register">
-//           Register
-//         </Link>
-//       </p>
-
-//     </form>
-    
-//   </div>
-// </div>
 
 <div className="w-full min-h-screen bg-gradient-to-br from-slate-100 via-white to-slate-100 flex items-center justify-center px-4">
   <div className="w-full max-w-md bg-white border border-slate-200 rounded-3xl shadow-xl p-6 sm:p-8">
